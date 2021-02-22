bceb1f8f152f8e79e863d48d06d33863c6d3f2f9
Badzso <norbert.ujj@gmail.com>
2021-02-22 12:04:24 +0100

    js file

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
 const menuBtn = document.querySelector('.menu-btn');
 const navbar = document.querySelector(".navbar");
-console.log(typeof(menuBtn),menuBtn);
+const main = document.querySelector(".main")
+const about = document.querySelector(".about");
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
     if(!menuOpen){
         menuBtn.classList.add('open');
         navbar.classList.add('jobbra');
+        main.classList.add("tuny");
+        about.setAttribute("style", "display: none");
         menuOpen = true;
     } else {
         menuBtn.classList.remove('open');
         navbar.classList.remove('jobbra');
+        main.classList.remove("tuny");
+        about.setAttribute("style", "display: normal");
         menuOpen = false;
     }
 });
