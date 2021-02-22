73ee15054e0075badf225a8946bf84933689db3b
Badzso <norbert.ujj@gmail.com>
2021-02-22 12:07:54 +0100

    main js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,15 @@
 const menuBtn = document.querySelector('.menu-btn');
 const navbar = document.querySelector(".navbar");
-const main = document.querySelector(".main")
-const about = document.querySelector(".about");
+console.log(typeof(menuBtn),menuBtn);
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
     if(!menuOpen){
         menuBtn.classList.add('open');
         navbar.classList.add('jobbra');
-        main.classList.add("tuny");
-        about.setAttribute("style", "display: none");
         menuOpen = true;
     } else {
         menuBtn.classList.remove('open');
         navbar.classList.remove('jobbra');
-        main.classList.remove("tuny");
-        about.setAttribute("style", "display: normal");
         menuOpen = false;
     }
 });
